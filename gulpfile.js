645e7d660a221f744c4774d592f9b82e32e2048c
Sander van Ast <sandervanast1@gmail.com>
2020-10-08 09:36:05 +0200

    :wrench: updated file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 const gulp = require('gulp');
+const { task } = require('gulp');
 const sass = require('gulp-sass');
 
 function style() {
     // 1. where is my scss files
-    return gulp.src('./scss/*.scss')
+    return gulp.src('./src/*.scss')
     // 2. pass that file through sass compiler
     .pipe(sass())
     // 3. where do I save the compiled css?
